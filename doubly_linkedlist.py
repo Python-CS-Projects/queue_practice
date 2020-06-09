cfbb3ecaafb5659626418c4dacc5bb72ff1b0455
fritzgt <tuanis@live.com>
2020-06-09 09:49:41 +0300

    init classes

--- doubly_linkedlist.py
+++ doubly_linkedlist.py
@@ -1,1 +1,21 @@
-class 
+'''
+Implementing a Doubly LL to use on the Queue and stack practices
+'''
+
+
+class Node:
+    def __init__(self, value, prev=None, next=None):
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+    # Helper methods below
+
+
+class DoublyLinkedList:
+    def __init__(self,node=None):
+        self.head = node
+        self.tail = node
+        self.count = 1 if node is not None else 0
+
+    #Helper methods below
