c98b117f698fb06c240f791423f08cb3df78625e
fritzgt <tuanis@live.com>
2020-06-11 12:30:03 +0300

    created BST

---
+++ binary_tree.py
@@ -0,0 +1,16 @@
+import doubly_linkedlist as dll
+
+
+class BST:
+    def __init__(self):
+        self.list = dll.DoublyLinkedList()
+
+    # Methods
+    def insert(self, value):
+        pass
+
+    def contains(self, value):
+        pass
+
+    def get_max(self):
+        pass
